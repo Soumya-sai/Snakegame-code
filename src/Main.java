6574cc8e0dd6e9b294bfae8b9672a4f75e992e07
Snakegame-code <soumyasai2014@gmail.com>
2023-07-16 20:50:53 +0530

    code changes for snakegame

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    Main(){
+        super("Sanke Game");
+        add(new Board());
+        pack();
+
+
+ //       setSize(300,300);
+        setLocationRelativeTo(null);
+        setResizable(false);
+ //       setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+      new Main().setVisible(true);
+    }
+}
